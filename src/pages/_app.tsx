1d0b5e5053d11a49d7eb9a7721c8f39c16ecf2a9
AntonioNarcilio <antonionarcilio@gmail.com>
2021-03-04 22:13:49 -0300

    🎉 build(ssr/styled-components): configurando de forma correta, para que o app utilize ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { ThemeProvider } from 'styled-components';
 
+import Head from 'next/head';
 import dracula from '../styles/themes/dracula';
 import GlocalStyles from '../styles/global';
 import { SelectPageProvider } from '../contexts/SelectPageButtonContext';
@@ -9,10 +10,20 @@ import { SelectPageProvider } from '../contexts/SelectPageButtonContext';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={dracula}>
+
+      <Head>
+        <link rel="icon" type="image/png" href="favicon.png" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+      </Head>
+
       <GlocalStyles />
+
       <SelectPageProvider>
         <Component {...pageProps} />
       </SelectPageProvider>
+
     </ThemeProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,49 @@
-import Document, {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+/* eslint-disable react/jsx-props-no-spreading */
+import Document from 'next/document';
+
+import { ServerStyleSheet } from 'styled-components';
+
+// export default class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel="icon" type="image/png" href="favicon.png" />
+
+//           <link rel="preconnect" href="https://fonts.gstatic.com" />
+//           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" type="image/png" href="favicon.png" />
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
