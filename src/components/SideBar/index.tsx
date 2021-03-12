31297f477e4f265a4432a84f811f33c1e18d2f17
AntonioNarcilio <antonionarcilio@gmail.com>
2021-03-12 11:33:20 -0300

    🎁 feat(switch): estilizando botao de seleção de tema

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,10 +2,10 @@ import SvgLogo from 'svg-react-loader!../../../public/icons/logo.svg';
 import SvgHome from 'svg-react-loader!../../../public/icons/home.svg';
 import SvgAward from 'svg-react-loader!../../../public/icons/award.svg';
 
-import SvgSun from 'svg-react-loader!../../../public/icons/sun-02.svg';
-import SvgSunActive from 'svg-react-loader!../../../public/icons/sun-01.svg';
+import SvgSun from 'svg-react-loader!../../../public/icons/sun.svg';
+import SvgSunActive from 'svg-react-loader!../../../public/icons/sun-active.svg';
 import SvgMoon from 'svg-react-loader!../../../public/icons/moon.svg';
-import SvgMoonActive from 'svg-react-loader!../../../public/icons/crescent-moon-01.svg';
+import SvgMoonActive from 'svg-react-loader!../../../public/icons/moon-active.svg';
 
 import Link from 'next/link';
 
@@ -100,10 +100,7 @@ export function SideBar({ toggleTheme }: SideBarProps) {
           <Switch
             onChange={toggleTheme}
             checked={title === 'dracula'}
-            // activeBoxShadow="#00fff2"
-            // uncheckedIcon={false}
-            // checkedIcon={false}
-            // boxShadow="0px 0px 4px 1px rgb(65, 65, 65)"
+            boxShadow="0px 0px 4px 1px rgba(65, 65, 65, 0.644)"
             activeBoxShadow="0px 0px 1px 4px rgba(255, 238, 0, 0)"
             uncheckedIcon={(
               <SvgMoon
@@ -112,7 +109,8 @@ export function SideBar({ toggleTheme }: SideBarProps) {
                   justifyContent: 'center',
                   alignItems: 'center',
                   height: '100%',
-                  width: '120%',
+                  width: '130%',
+                  padding: '1.5px',
                 }}
               />
             )}
@@ -124,6 +122,7 @@ export function SideBar({ toggleTheme }: SideBarProps) {
                   alignItems: 'center',
                   height: '100%',
                   width: '75%',
+                  padding: '1.2px',
                 }}
               />
             )}
@@ -152,8 +151,6 @@ export function SideBar({ toggleTheme }: SideBarProps) {
             height={18}
             width={45}
             handleDiameter={20}
-            // offColor={colors.sideBarDisable}
-            // onColor={colors.sideBarDisable}
             offColor="#393A59"
             onColor="#008FE0"
           />
